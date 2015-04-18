ad8f2a1885891c9db25b87d7612b55c3e8b491a3
highsource <aleksei.valikov@gmail.com>
2015-04-18 07:48:43 +0200

    Related to highsource/ogc-schemas#89.

--- o/ogc-schemas-browserify/README.md
+++ o/ogc-schemas-browserify/README.md
@@ -1,34 +1,20 @@
 ## Install
 
-Install deps with
+Install deps with:
 
     npm install
 
 ## Node version
 
-Then, to test my test for node version, do
+Run in Node.js:
 
     npm run-script node_ok
 
-It works
-
 ## Browserify
 
-In Browserify,  I encounter two issues in fact. I only asked on the second (https://github.com/highsource/ogc-schemas/issues/89) as I was able to solve the first one.
-
-You can see the first one by running
+Build the bunlde with Browserify:
 
     npm run-script build_browserify
 
+The start in browser - either with a local web server or, in Chrome with `chrome --allow-file-access-from-files`.
 
-and opening index.html
-
-IMO, The build is unable to load correctly each file because node_modules/w3c-schemas/w3c-schemas.js load dynamicly each subfile but it's not something Browserify can solve
-
-and the second one with
-
-    npm run-script build_browserify_hack
-
-I use a process to add files to solve the dependency issue using `cat`. See in package.json
-
-I open the browser and I get the issue described in https://github.com/highsource/ogc-schemas/issues/89 
--- o/ogc-schemas-browserify/js/app.js
+++ o/ogc-schemas-browserify/js/app.js
@@ -1,24 +1,19 @@
 var Jsonix = require('jsonix').Jsonix;
-// Below require will make a build unable to load correctly each file because
-// node_modules/w3c-schemas/w3c-schemas.js load dynamicly each subfile but it's not something Browserify can solve
-var XLink_1_0 = require('w3c-schemas').XLink_1_0;
-var OWS_1_1_0 = require('ogc-schemas').OWS_1_1_0;
-var WMS_1_3_0 = require('ogc-schemas').WMS_1_3_0;
+var XLink_1_0 = require('w3c-schemas/lib/XLink_1_0').XLink_1_0;
+var OWS_1_1_0 = require('ogc-schemas/lib/OWS_1_1_0').OWS_1_1_0;
+var WMS_1_3_0 = require('ogc-schemas/lib/WMS_1_3_0').WMS_1_3_0;
 
-// Browser version take
-var toUnmarshal = [
-  XLink_1_0,
-  OWS_1_1_0,
-  WMS_1_3_0
-];
-
-var context = new Jsonix.Context(toUnmarshal, {
-  namespacePrefixes: {
-    'http://www.opengis.net/wms': 'wms'
-  }
+var context = new Jsonix.Context([XLink_1_0, OWS_1_1_0, WMS_1_3_0], {
+	// Not required but recommended
+	namespacePrefixes: {
+		'http://www.w3.org/1999/xlink': 'xlink',
+		'http://www.opengis.net/wms': 'wms',
+		'http://www.opengis.net/ows/1.1': 'ows'
+	}
 });
+
 var unmarshaller = context.createUnmarshaller();
 
 unmarshaller.unmarshalURL('data/ogcsample.xml', function(result) {
-  console.log(JSON.stringify(result, null, '  '));
+	console.log(JSON.stringify(result, null, 2));
 });

e173ea0a5a97b07bf6a461621b11c68520a89e3f
highsource <aleksei.valikov@gmail.com>
2015-12-22 22:45:47 +0100

    Issue highsource/jsonix#114.

--- u/ubl/index.js
+++ u/ubl/index.js
@@ -1,4 +1,18 @@
 var Jsonix = require('jsonix').Jsonix;
-var Invoice = require('./mappings/Invoice').Invoice;
 
-new Jsonix.Context([Invoice]);
+var mappings = [
+	require('./mappings/UBL_XAdESv132').UBL_XAdESv132,
+	require('./mappings/UBL_XAdESv141').UBL_XAdESv141,
+	require('./mappings/UBL_DS').UBL_DS,
+	require('./mappings/UBL_CAC').UBL_CAC,
+	require('./mappings/UBL_CBC').UBL_CBC,
+	require('./mappings/UBL_CEC').UBL_CEC,
+	require('./mappings/UBL_CSC').UBL_CSC,
+	require('./mappings/UBL_SAC').UBL_SAC,
+	require('./mappings/UBL_SBC').UBL_SBC,
+	require('./mappings/UBL_UDT').UBL_UDT,
+	require('./mappings/CCTS_CCT').CCTS_CCT,
+	require('./mappings/UBL_Invoice').UBL_Invoice
+];
+
+var context = new Jsonix.Context(mappings);
